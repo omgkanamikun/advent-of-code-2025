ba6bc1ef605195079f6b5e791ee6a38ff83563b7
Vladyslav Kondratenko <vladyslav.kondratenko@ibm.com>
2025-12-16 17:34:36 +0100

    Added implementation for the second part of the Day 1 with tests

--- task_1_safe_puzzle/src/main.rs
+++ task_1_safe_puzzle/src/main.rs
@@ -140,7 +140,27 @@ impl SafeDialKnob {
         SafeDialKnob::default()
     }
 
-    fn rotate_knob(&mut self, command: &RotationCommand) {
+    fn rotate_knob_solution_two(&mut self, command: &RotationCommand) {
+        let mut current: i32 = self.current_position;
+        let direction = &command.direction;
+        let mut steps: i32 = command.distance;
+
+        while steps != 0 {
+            current = match direction {
+                Direction::Right => (current + 1) % 100,
+                Direction::Left => (current - 1) % 100,
+            };
+
+            if current == 0 {
+                self.zero_position_occurrence += 1;
+            }
+
+            steps -= 1;
+        }
+        self.current_position = current;
+    }
+
+    fn rotate_knob_solution_one(&mut self, command: &RotationCommand) {
         let mut current: i32 = self.current_position;
         let direction = &command.direction;
         let steps: i32 = command.distance;
@@ -157,17 +177,41 @@ impl SafeDialKnob {
         self.current_position = current;
     }
 
-    fn execute_rotation_commands(&mut self, commands: &[RotationCommand]) {
-        for command in commands {
-            self.rotate_knob(command);
-        }
+    fn apply_rotation_commands_solution_two(&mut self, commands: &[RotationCommand]) {
+        commands
+            .iter()
+            .for_each(|command| self.rotate_knob_solution_two(command));
     }
 
-    fn get_code_sequence(&self) -> u32 {
+    fn apply_rotation_commands_solution_one(&mut self, commands: &[RotationCommand]) {
+        commands
+            .iter()
+            .for_each(|command| self.rotate_knob_solution_one(command));
+    }
+
+    fn get_code_sequence(self) -> u32 {
         self.zero_position_occurrence
     }
 }
 
+/// --- Day 1: Secret Entrance ---
+///
+/// The Elves have good news and bad news.
+///
+/// The good news is that they've discovered project management! This has given them the tools they need to prevent their usual Christmas emergency. For example, they now know that the North Pole decorations need to be finished soon so that other critical tasks can start on time.
+///
+/// The bad news is that they've realized they have a different emergency: according to their resource planning, none of them have any time left to decorate the North Pole!
+///
+/// To save Christmas, the Elves need you to finish decorating the North Pole by December 12th.
+///
+/// Collect stars by solving puzzles. Two puzzles will be made available on each day; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+///
+/// You arrive at the secret entrance to the North Pole base ready to start decorating. Unfortunately, the password seems to have been changed, so you can't get in. A document taped to the wall helpfully explains:
+///
+/// "Due to new security protocols, the password is locked in the safe below. Please see the attached document for the new combination."
+///
+/// The safe has a dial with only an arrow on it; around the dial are the numbers 0 through 99 in order. As you turn the dial, it makes a small click noise as it reaches each number.
+///
 /// The attached document (your puzzle input) contains a sequence of rotations, one per line, which tell you how to open the safe. A rotation starts with an L or R which indicates whether the rotation should be to the left (toward lower numbers) or to the right (toward higher numbers). Then, the rotation has a distance value which indicates how many clicks the dial should be rotated in that direction.
 ///
 /// So, if the dial were pointing at 11, a rotation of R8 would cause the dial to point at 19. After that, a rotation of L19 would cause it to point at 0.
@@ -207,18 +251,59 @@ impl SafeDialKnob {
 /// The dial is rotated R14 to a point at 14.
 /// The dial is rotated L82 to a point at 32.
 /// Because the dial points at 0 a total of three times during this process, the password in this example is 3.
+/// Analyze the rotations in your attached document. What's the actual password to open the door?
+///
+/// --- Part Two ---
+///
+/// You're sure that's the right password, but the door won't open. You knock, but nobody answers. You build a snowman while you think.
+///
+/// As you're rolling the snowballs for your snowman, you find another security document that must have fallen into the snow:
+///
+/// "Due to newer security protocols, please use password method 0x434C49434B until further notice."
+///
+/// You remember from the training seminar that "method 0x434C49434B" means you're actually supposed to count the number of times any click causes the dial to point at 0, regardless of whether it happens during a rotation or at the end of one.
+///
+/// Following the same rotations as in the above example, the dial points at zero a few extra times during its rotations:
+///
+/// The dial starts by pointing at 50.
+/// The dial is rotated L68 to point at 82; during this rotation, it points at 0 once.
+/// The dial is rotated L30 to a point at 52.
+/// The dial is rotated R48 to a point at 0.
+/// The dial is rotated L5 to a point at 95.
+/// The dial is rotated R60 to point at 55; during this rotation, it points at 0 once.
+/// The dial is rotated L55 to a point at 0.
+/// The dial is rotated L1 to a point at 99.
+/// The dial is rotated L99 to a point at 0.
+/// The dial is rotated R14 to a point at 14.
+/// The dial is rotated L82 to point at 32; during this rotation, it points at 0 once.
+/// In this example, the dial points at 0 three times at the end of a rotation, plus three more times during a rotation. So, in this example, the new password would be 6.
+///
+/// Be careful: if the dial were pointing at 50, a single rotation like R1000 would cause the dial to point at 0 ten times before returning back to 50!
+///
+/// Using password method 0x434C49434B, what is the password to open the door?
 fn main() -> anyhow::Result<()> {
-    let rotation_commands = load_rotation_commands("input").with_context(|| "failed in main")?;
+    let rotation_commands =
+        load_rotation_commands("puzzle_input").with_context(|| "failed in main")?;
 
     if rotation_commands.is_empty() {
         bail!("no commands to execute");
     }
 
     let mut safe_knob = SafeDialKnob::init();
+    safe_knob.apply_rotation_commands_solution_one(&rotation_commands);
 
-    safe_knob.execute_rotation_commands(&rotation_commands);
+    println!(
+        "The code for the fist puzzle, solution one is: {}",
+        safe_knob.get_code_sequence()
+    );
 
-    println!("The code is: {}", safe_knob.get_code_sequence());
+    let mut safe_knob = SafeDialKnob::init();
+    safe_knob.apply_rotation_commands_solution_two(&rotation_commands);
+
+    println!(
+        "The code for the first puzzle, solution two is: {}",
+        safe_knob.get_code_sequence()
+    );
 
     Ok(())
 }
@@ -301,20 +386,42 @@ mod tests {
     }
 
     #[test]
-    fn test_solution_small_input() {
+    fn test_solution_one_small_puzzle_input() {
+        let first_expected_answer = 3;
         let test_puzzle_input = load_rotation_commands("test_input").unwrap();
         let mut safe = SafeDialKnob::init();
-        safe.execute_rotation_commands(&test_puzzle_input);
+        safe.apply_rotation_commands_solution_one(&test_puzzle_input);
 
-        assert_eq!(3, safe.get_code_sequence());
+        assert_eq!(first_expected_answer, safe.get_code_sequence());
     }
 
     #[test]
-    fn test_solution_main_puzzle_input() {
-        let test_puzzle_input = load_rotation_commands("input").unwrap();
+    fn test_solution_two_small_puzzle_input() {
+        let second_expected_answer = 6;
+        let test_puzzle_input = load_rotation_commands("test_input").unwrap();
         let mut safe = SafeDialKnob::init();
-        safe.execute_rotation_commands(&test_puzzle_input);
+        safe.apply_rotation_commands_solution_two(&test_puzzle_input);
 
-        assert_eq!(1135, safe.get_code_sequence());
+        assert_eq!(second_expected_answer, safe.get_code_sequence());
+    }
+
+    #[test]
+    fn test_solution_one_puzzle_input() {
+        let first_star_answer = 1135;
+        let test_puzzle_input = load_rotation_commands("puzzle_input").unwrap();
+        let mut safe = SafeDialKnob::init();
+        safe.apply_rotation_commands_solution_one(&test_puzzle_input);
+
+        assert_eq!(first_star_answer, safe.get_code_sequence());
+    }
+
+    #[test]
+    fn test_solution_two_puzzle_input() {
+        let second_start_answer = 6558;
+        let test_puzzle_input = load_rotation_commands("puzzle_input").unwrap();
+        let mut safe = SafeDialKnob::init();
+        safe.apply_rotation_commands_solution_two(&test_puzzle_input);
+
+        assert_eq!(second_start_answer, safe.get_code_sequence());
     }
 }
