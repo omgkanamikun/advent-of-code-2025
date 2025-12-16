f854166cebbb572f898f950b16ed48417b177b0d
Vladyslav Kondratenko <vladyslav.kondratenko.dev@gmail.com>
2025-12-16 18:13:25 +0100

    Added Day 2: Gift Shop structure for the second day AoC 2025 challenge

---
+++ day_2_gift_shop/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Advent of Code 2025");
+}
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    #[test]
+    fn test_rotation_command_right_direction() {
+        assert_eq!(12, 12);
+    }
+}
